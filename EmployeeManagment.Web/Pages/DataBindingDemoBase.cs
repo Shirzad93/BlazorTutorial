c7af641529bdc7ce8d423c1a0a4e6f5e13a4ae58
Shirzad <shirzad_1993@hotmail.com>
2020-07-23 14:38:29 +0200

    Data binding two ways

---
+++ EmployeeManagment.Web/Pages/DataBindingDemoBase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagment.Web.Pages
+{
+    public class DataBindingDemoBase : ComponentBase
+    {
+        protected string Name { get; set; } = "Tom";
+        protected string Gender { get; set; } = "Male";
+        protected string color { get; set; } = "background-color:white";
+    }
+}
